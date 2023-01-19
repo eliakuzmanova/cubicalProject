dcb7da57d76765d9d5f41682207f1897cb42e2e6
Elia Kuzmanova <eliamitova7@gmail.com>
2023-01-20 00:47:29 +0100

    set up the express

--- config/express.js
+++ config/express.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+
 
 module.exports = (app) => {
-    
-    //TODO: Setup the view engine
 
-    //TODO: Setup the body parser
+    app.engine("hbs", handlebars.engine({
+        extname: ".hbs"
+    }));
+   
+    app.set('view engine', "hbs");
 
-    //TODO: Setup the static files
-
+    app.use(express.static("public"));
 };
