6308200a897d1a22d4396210cf0d4ad6caee1ca1
Elia Kuzmanova <eliamitova7@gmail.com>
2023-01-22 15:26:37 +0100

    home look without functionalities

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const env = process.env.NODE_ENV || 'development';
 
 const config = require('./config/config')[env];
+const cubeController  = require('./controllers/cubeController')
+const homeController = require('./controllers/homeController')
+
 const app = require('express')();
 
 require('./config/express')(app);
 require('./config/routes')(app);
+
+
+app.get("/", homeController.getHomeView)
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
